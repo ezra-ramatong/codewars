99e2e86c101a910e29d006f69b082639338215c7
Ezra Ramatong <128799204+ezra-ramatong@users.noreply.github.com>
2023-10-05 13:42:40 +0200

    opposites attract

---
+++ javascript/8kyu/opposites-attract.js
@@ -0,0 +1,16 @@
+/* function isEven(i) {
+  return (i % 2 === 0) ? true : false;
+}
+
+function lovefunc(flower1, flower2) {
+  // moment of truth
+  if (isEven(flower1) && !isEven(flower2) ||
+    isEven(flower2) && !isEven(flower1)) {
+    return true;
+  } else {
+    return false;
+  }
+} */
+
+
+console.log(452 % 2 !== 452 % 2)
