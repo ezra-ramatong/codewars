147fb0d3de94ae4669dee12b3c4423040ea7232c
Ezra Ramatong <128799204+ezra-ramatong@users.noreply.github.com>
2023-10-20 12:28:02 +0200

    move to js

--- c++/8kyu/all-star-18.js
+++
@@ -1,4 +0,0 @@
-// All Star Code Challenge #18
-function strCount(str, letter){  
-  return str.split('').filter(x => x === letter).length;
-}
--- c++/8kyu/reversed-strings.js
+++
@@ -1,4 +0,0 @@
-// Reversed Strings
-function solution(str) {
-  return str.split('').reverse().join('');
-}
