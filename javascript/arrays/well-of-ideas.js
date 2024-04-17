cd41eebf810dab0f89103489ecbbc423de3cdec9
Ezra Ramatong <128799204+ezra-ramatong@users.noreply.github.com>
2024-04-17 16:39:44 +0200

    forgot to use git mv instead

--- javascript/arrays/well-of-ideas.js
+++
@@ -1,8 +0,0 @@
-/* Well of Ideas - Easy Version (8kyu)*/
-
-function well(array) {
-  if (!array.includes("good")) return "Fail!";
-  if (array.filter((element) => element === "good").length > 2) {
-    return "I smell a series!";
-  } else return "Publish!";
-}
